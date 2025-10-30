6d4ee1782836154344175b51fe2942e295c29d36
Peter Robertson <pdrbertson@gmail.com>
2025-10-30 16:51:51 -0700

    Added game objects

--- MilleBornes/Program.cs
+++ MilleBornes/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+using System.Security;
+
 Console.WriteLine("Hello, World!");
+Player playerA = new()
+{
+    Name = "Peter"
+};
+Player playerB = new()
+{
+    Name = "Tim"
+};
+public class Player
+{
+    public string? Name { get; set; }
+    public Card[]? HandStack { get; set; }
+    public PlayerArea? TheArea { get; set; }
+}
+public class PlayerArea
+{
+    public Card[]? LightStack { get; set; }
+    public Card[]? LimitStack { get; set; }
+    public Card[]? DistanceStack { get; set; }
+    public Card[]? SpecialStack { get; set; }
+}
+
+public class Card
+{
+    public string Name { get; set; }
+    Card(string name)
+    {
+        Name = name;
+    }
+    
+}
